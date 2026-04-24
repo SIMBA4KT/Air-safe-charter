0c53e367d684ec91e96ae4504ad6731806995830
SIMBA <simbajames40@gmail.com>
2026-04-24 15:59:46 +0300

    Sceond commit: fixed header section.

--- air-safe-frontend/src/App.vue
+++ air-safe-frontend/src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <Header/> <main class="h-[200vh] bg-slate-900">
+    <section class="pt-32 text-center text-white">
+      <h1 class="text-4xl font-bold">Welcome to Air-Safe</h1>
+      <p>Scroll down to see the header change!</p>
+    </section>
+  </main>
 </template>
+
+<style>
+
+body {
+  margin: 0;
+  background-color: #0f172a; 
+  color: #e0e0e0;
+  font-family: 'Inter', sans-serif;
+}
+</style>
